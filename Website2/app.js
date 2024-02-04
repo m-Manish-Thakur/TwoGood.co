7c79677b068b9b078bab3b86e208a883522fa6f0
Manish Thakur <manishthakur231690@gmail.com>
2024-02-04 16:09:01 +0530

    Added

--- Website2/app.js
+++ Website2/app.js
@@ -3,8 +3,17 @@ const init = () => {
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true,
+    smartphone: { smooth: true },
+    tablet: { smooth: true },
+    lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
+    multiplier: 1.2,
   });
-  locoScroll.on("scroll", ScrollTrigger.update);
+
+  locoScroll.on("scroll", () => {
+    console.log("LocoScroll is scrolling");
+    ScrollTrigger.update();
+  });
+
   ScrollTrigger.scrollerProxy("#main", {
     scrollTop(value) {
       return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
@@ -14,10 +23,26 @@ const init = () => {
     },
     pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
   });
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.addEventListener("refresh", () => {
+    console.log("ScrollTrigger is refreshed");
+    locoScroll.update();
+  });
+
   ScrollTrigger.refresh();
 };
-init();
+// init();
+
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {});
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
 
 const tl = gsap.timeline();
 
@@ -54,7 +79,7 @@ gsap.to("#header h1", {
   opacity: 0,
   scrollTrigger: {
     trigger: "#header h1",
-    scroller: "#main",
+    scroller: "body",
     start: "top 5%",
     scrub: 2,
   },
@@ -102,7 +127,7 @@ gsap.from("#page2 .elem1 p", {
   opacity: 0,
   scrollTrigger: {
     trigger: "#page2 .elem1 p",
-    scroller: "#main",
+    scroller: "body",
     start: "top 60%",
     end: "top 40%",
     scrub: 2,
@@ -110,17 +135,75 @@ gsap.from("#page2 .elem1 p", {
 });
 
 gsap.from("#page2 #elem2 p", {
-  y: "51px",
+  y: "155px",
   duration: 1,
-  stagger: 0.1,
+  stagger: 0.25,
   opacity: 0,
   scrollTrigger: {
-    trigger: "#page2 #elem2 p",
-    scroller: "#main",
+    trigger: "#page2",
+    scroller: "body",
     start: "top 60%",
-    end: "top 0%",
+    end: "top 10%",
     scrub: 2,
   },
 });
 
-// ##########################   Page 3   ##############################
+// ##########################   Page 4   ##############################
+
+gsap.from("#page4 .elem1 p", {
+  y: "100%",
+  duration: 0.3,
+  stagger: 0.2,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page4 .elem1 p",
+    scroller: "body",
+    start: "top 60%",
+    end: "top 40%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#page4 #elem2 p", {
+  y: "155px",
+  duration: 1,
+  stagger: 0.25,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page4",
+    scroller: "body",
+    start: "top 60%",
+    end: "top 10%",
+    scrub: 2,
+  },
+});
+
+// ##################################   Page 5  ######################
+
+// ##################################   Get In Touch   ####################
+
+gsap.from("#getintouch h1", {
+  y: 55,
+  duration: 1,
+  delay: 0.1,
+  yoyo: true,
+  scrollTrigger: {
+    trigger: "#getintouch h1",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 35%",
+    scrub: 3,
+  },
+});
+
+// --------------   Swiper js   --------------------
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+});
